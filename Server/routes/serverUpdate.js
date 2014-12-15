b2e68d3d43b111d2fa5c2e03029405353f087ede
Nils Neumann <N_Neumann@outlook.de>
2014-12-15 16:09:02 +0100

    Added Sys and fixed ServerUpdate

--- Server/routes/serverUpdate.js
+++ Server/routes/serverUpdate.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/execute', function(req, res) {
+console.log("Start Script!");
 var sys = require('sys');
 var exec = require('child_process').exec;
  
 function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("/home/serverDev/update.sh.sh", puts);
+exec("/home/serverDev/update.sh", puts);
 res.send("");
 });
 
