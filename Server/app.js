6c8f85d438362fe76f1924fddb9df6210dd33cc3
TimoBlock <kernproblem@live.de>
2015-01-23 17:05:43 +0100

    added var parseurl=require('parseurl');

--- Server/app.js
+++ Server/app.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var stylus = require('stylus');
 var nib = require('nib');
+var parseurl = require('parseurl'); 
 
 // Database
 var mongo = require('mongoskin');
